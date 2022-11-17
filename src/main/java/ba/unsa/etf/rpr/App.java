befacf3f27b20a20556e0e8cb5d45cd9fc321434
Nejra <nadilovic2@etf.unsa.ba>
2022-11-17 17:36:08 +0100

    Zadaca iz predmeta "Razvoj programskih rješenja"

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -15,15 +15,19 @@ public class App
      * @param args array of sequence of characters (Strings) that are passed to the main function
      */
     public static void main( String[] args ) {
-        if (!Objects.equals(args[0], "(")) {
-            throw new RuntimeException(errorMessage);
+        try {
+            if (!Objects.equals(args[0], "(")) {
+                throw new RuntimeException(errorMessage);
+            }
+            StringBuilder entry = new StringBuilder();
+            for (String x : args) {
+                validExpression(x);
+                entry.append(x).append(" ");
+            }
+            double result = evaluate(String.valueOf(entry));
+            System.out.println("The result using \"Dijkstra's Algorithm for expression evaluation\" is " + result);
+        }catch (RuntimeException e){
+            System.out.println(e);
         }
-        StringBuilder entry = new StringBuilder();
-        for(String x : args) {
-            validExpression(x);
-            entry.append(x).append(" ");
-        }
-        double result = evaluate(String.valueOf(entry));
-        System.out.println("The result using \"Dijkstra's Algorithm for expression evaluation\" is " + result);
     }
 }
